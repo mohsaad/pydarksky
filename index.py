223cd37ad358873a3a161ecb18fbe7353100fcb0
mohsaad <msaad96@gmail.com>
2019-04-07 01:24:41 -0700

    more updates

--- index.py
+++ index.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+from datetime import datetime
 import argparse
 import requests
 import json
 import os
 
+
 LOCATION_URL = "http://www.cleardarksky.com/t/chart_prop00.txt"
 BASE_URL = "http://www.cleardarksky.com/txtc"
+
 
 class ClearDarkSkyData():
 
@@ -24,12 +27,12 @@ class ClearDarkSkyData():
     def _download_locations(self):
         if not self._check_for_existing_locations():
             r = requests.get(LOCATION_URL)
-            open(self.location_download_path, 'w').write(r.content)
+            open(self.location_download_path, 'wb').write(r.content)
 
 
     def _build_or_load_location_map(self):
         self._download_locations()
-        f = open('/tmp/sky_locations.txt')
+        f = open('/tmp/sky_locations.txt', encoding = "ISO-8859-1")
         for line in f:
             key, state, location = line.split('\n')[0].split('|')
             if not state.lower() in self.locations:
@@ -46,7 +49,7 @@ class ClearDarkSkyData():
     def download_sky_chart(self, key):
         url = '{}/{}csp.txt'.format(BASE_URL, key)
         r = requests.get(url)
-        open('/tmp/{}.txt'.format(key), 'w').write(r.content)
+        open('/tmp/{}.txt'.format(key), 'wb').write(r.content)
 
         return "/tmp/{}.txt".format(key)
 
@@ -56,17 +59,48 @@ class ClearDarkSkyData():
         version = f.readline().strip(' ').split('=')[1].strip('"')
         UTC_offset = f.readline().strip(' ').split('=')[1]
           
-        transparency_values = []
+        transparency_values = {}
   
         # Skip the next 4 lines.
         for i in range(0, 4):
           f.readline()
+        
+        line = 'n'
+        while line != None: 
+            line = f.readline()
+            if line.split('\n')[0] == ')':
+                break
+            
+            filter_text = str.maketrans('"()\n\t', "       ")
+            values = line.translate(filter_text).split(',')[0:-2]
+            datetime_obj = datetime.strptime(values[0], '%Y-%m-%d %H:%M:%S')
+            transparency_values[datetime_obj] = values[1:-1]
 
-        for line in f:
-          if line.split('\n')[0] == ')':
-            break
-          
-          print(line.split('\n')[0])
+        
+        # Get all data after next 5 lines.
+        
+        for i in range(0, 5):
+            f.readline()
+
+        while line != None:
+            line = f.readline()
+            if line.split('\n')[0] == ')':
+                break
+
+            values = line.translate(filter_text).split(',') 
+            datetime_obj = datetime.strptime(values[0], '%Y-%m-%d %H:%M:%S')
+      
+            if datetime_obj in transparency_values:
+                transparency_values[datetime_obj].append(values[1:-1])
+
+        for key in transparency_values:
+            transparency_values[key] = [item for sublist in transparency_values[key] for item in sublist]
+
+        return transparency_values
+
+    def print_transparency_values(self, transparency_values):
+      print(u'\u25a0')  
+      print(transparency_values)
 
 def main():
     parser = argparse.ArgumentParser(description="Get clear sky charts and display them in the terminal")
@@ -74,7 +108,8 @@ def main():
 
     c = ClearDarkSkyData()
     link = c.download_sky_chart('SanFranCA')
-    c.interpret_sky_chart(link)
+    vals = c.interpret_sky_chart(link)
+    c.print_transparency_values(vals)
 
 if __name__ == '__main__':
     main()
