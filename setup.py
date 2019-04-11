333ba1c3bc84c1da8b0e6f0e1ba63d962e189169
mohsaad <msaad96@gmail.com>
2019-04-10 23:13:06 -0700

    Added options to setup.py

--- setup.py
+++ setup.py
@@ -9,20 +9,23 @@ with open(path.join(current_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pycsk',
-    version = '0.1.2',
+    version = '0.2.0',
+    description="Clear sky charts in your terminal",
     packages = ['pycsk'],
     entry_points = {
       'console_scripts': [
           'csk = pycsk.__main__:main'
       ]},
     install_requires = [
-      'sty',
-      'appdirs'
+      'requests>=2.4.18',
+      'sty>=1.0.0b9',
+      'appdirs>=1.4.3'
     ],
     license = 'MIT',
     url = 'https://github.com/mohsaad/pydarksky',
     author='Mohammad Saad',
     author_email='mohammad.saad@outlook.com',
     long_description=long_description,
+    python_requires=">=3.6",
     long_description_content_type='text/markdown'
 )
